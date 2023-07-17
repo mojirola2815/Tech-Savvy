47d0e90270ec521b670755e4464ad35b447395f0
mojirola2815 <136636866+mojirola2815@users.noreply.github.com>
2023-07-17 10:27:00 +0700

    Fix broken links

---
+++ nature_king_island.kt
@@ -0,0 +1,30 @@
+fun main(args: Array<String>) {
+    //1
+    var firstNumber: Int = 0
+
+    //2
+    var secondNumber: Int = 0
+
+    //3
+    while (firstNumber < 2000) {
+
+        //4
+        secondNumber = 0 
+
+        //5
+        while (secondNumber < 2000) {
+
+            //6
+            println("Tech Savvy")
+
+            //7
+            secondNumber++
+
+        }
+
+        //8
+        firstNumber++
+
+    }
+
+}
